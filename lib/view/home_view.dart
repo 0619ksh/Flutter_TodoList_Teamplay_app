7c04f0dea81d0fabe9101bd971a54de4150a9d53
Github jangeunhea <Github jangeunhea94@gmail.com>
2026-08-11 22:12:06 +0900

    Add home and camera views

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -10,6 +10,30 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xFFF4F7FF),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            '안녕하세요,   님',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '오늘도 한 걸음 가벼운 하루를 설계해보세요.',
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey
+            ),
+            )
+        ],
+      ),
+    );
   }
 }
