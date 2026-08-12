b254e92b1df16db5db57c088824f15386ae95886
SeunghyunKim <0619ksh23@gmail.com>
2026-08-12 10:40:44 +0900

    comment edit

--- lib/model/todo_list.dart
+++ lib/model/todo_list.dart
@@ -2,7 +2,7 @@ class TodoList {
   // Properties
   String todoText;  // 할 일
   DateTime date;    // 날짜
-  bool isImportant; // 중요 할 일 체크
+  bool isImportant; // 중요 여부
   bool isCompleted; // 완료 여부
 
   // Constructor
